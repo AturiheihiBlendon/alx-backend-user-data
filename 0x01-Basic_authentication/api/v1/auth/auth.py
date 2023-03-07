b717e4502d896818a6ea05992008296711b95288
AturiheihiBlendon <2020blendon@gmail.com>
2023-03-07 19:11:56 +0300

    Task 4 - routes that dont need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,9 +19,9 @@ class Auth():
         invalid_path = f"{path}/"
         if path is None:
             return True
-        if len(excluded_paths) == 0 or excluded_paths is None:
+        elif len(excluded_paths) == 0 or excluded_paths is None:
             return True
-        if path in excluded_paths or invalid_path in excluded_paths:
+        elif path in excluded_paths or invalid_path in excluded_paths:
             return False
         return True
 
