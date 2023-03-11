4a4dd66d7278f832b2b21ab8acb421e0ecbca07b
AturiheihiBlendon <2020blendon@gmail.com>
2023-03-11 06:13:12 +0300

    Task 5 - Update before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,12 +30,13 @@ def before_request():
     Filtering requests
     """
     ex_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                '/api/v1/forbidden/']
+                '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is None:
         return None
     if not auth.require_auth(request.path, ex_paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
