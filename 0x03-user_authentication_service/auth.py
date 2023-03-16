792cfad533eb759c022ced0bcadefa248558f073
AturiheihiBlendon <2020blendon@gmail.com>
2023-03-16 12:16:47 +0300

    Task 13 - Destroy user session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,3 +81,13 @@ class Auth:
                 return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroys user session
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+            return None
+        except Exception:
+            return None
