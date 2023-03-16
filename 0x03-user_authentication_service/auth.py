798f7c8b67d20cabbfc76dfc5c7ca86710039bb1
AturiheihiBlendon <2020blendon@gmail.com>
2023-03-16 12:08:55 +0300

    Task 12 - find user by id session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,6 +9,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 import uuid
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -65,5 +66,18 @@ class Auth:
             self._db.update_user(user.id, session_id=session_id)
             return session_id
 
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """
+        Finds and gets user by session id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            if user:
+                return user
         except NoResultFound:
             return None
